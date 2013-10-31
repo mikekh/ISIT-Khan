e1492e77657604de266d7d725f24df7c2372e795
unknown <shaheen1955@gmail.com>
2013-10-30 19:57:25 -0700

    added a more informative roll details

---
+++ Week05_RollDIce_03_Khan/index.js
@@ -0,0 +1,26 @@
+/**
+ * @author MIKE
+ */
+function diceRolls($scope) {
+	$scope.diceScoreStr = "";
+	$scope.total = 0;
+	
+	$scope.setDiceRolls = function(numbDice, numbSides){
+		//$scope.diceScoreStr = "";
+		$scope.diceScoreStr = setScoreStr(numbDice, numbSides);
+		$scope.total = 0;	
+		for(var i= 0; i < numbDice; i++){
+			var diceFace =	$scope.dice1 = rollDice(numbSides);
+			$scope.diceScoreStr = $scope.diceScoreStr + diceFace + ", ";
+			$scope.total = $scope.total + diceFace;
+		}
+	};
+};
+
+var rollDice = function(numbSides) {
+    return Math.floor(Math.random() * numbSides) + 1;
+};
+
+var setScoreStr = function(numbDice, numbSides){
+	return  numbDice + " Dice with " + numbSides +" sides Rolled: ";
+};
